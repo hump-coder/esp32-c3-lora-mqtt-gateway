306a96e59ee61d47869f1c648839bd990bbdfb17
hump-coder <karl.bridger@gmail.com>
2025-08-05 12:39:27 +0930

    feat: add gateway protocol helper for devices

--- examples/Relay/Relay.ino
+++ examples/Relay/Relay.ino
@@ -1,29 +1,26 @@
 #include <Arduino.h>
 #include "LoRaHandler.h"
+#include "GatewayProtocol.h"
 #include "DeviceTypes/RelayDevice.h"
 #include "config.h"
 
 LoRaHandler lora;
+GatewayProtocol gateway(lora, "relay1", {"relay"}, true);
 bool relayState = false;
 
 void setup() {
   Serial.begin(115200);
   lora.begin();
-  lora.sendPacket("relay1:register:relay");
+  gateway.onCommand([](const String &sensor, const String &value) {
+    if (sensor == "relay") {
+      relayState = RelayDevice::decode(value);
+      Serial.println(String("Relay state: ") + (relayState ? "ON" : "OFF"));
+    }
+  });
+  gateway.begin();
 }
 
 void loop() {
-  int rssi; float snr;
-  if (lora.available()) {
-    String pkt = lora.receivePacket(rssi, snr);
-    int first = pkt.indexOf(':');
-    int second = pkt.indexOf(':', first + 1);
-    String msgType = pkt.substring(first + 1, second);
-    String payload = pkt.substring(second + 1);
-    if (msgType == "cmd") {
-      relayState = RelayDevice::decode(payload);
-      Serial.println(String("Relay state: ") + (relayState ? "ON" : "OFF"));
-    }
-  }
+  gateway.loop();
 }
 
